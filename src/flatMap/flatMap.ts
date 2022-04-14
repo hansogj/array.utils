5594609dec3a93e18c0db2735d69464201d0b80a
Hans Ole Gjerdrum <hansogj@gmail.com>
2022-04-14 14:20:57 +0200

    feat(typedefinition) adding d.ts for flatMap

--- src/flatMap/flatMap.ts
+++ src/flatMap/flatMap.ts
@@ -1,3 +1,12 @@
+
+// eslint-disable-next-line @typescript-eslint/no-redeclare
+interface Array<T> {
+  flatMap<U, This = undefined> (
+    callback: (this: This, value: T, index: number, array: T[]) => U | ReadonlyArray<U>,
+    thisArg?: This
+  ): U[]
+}
+
 Array.prototype.flatMap =
   Array.prototype.flatMap ||
   function (lambda) {
